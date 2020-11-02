06cdeab6dec563f610c28d340d6ed954fc009977
Daniel Reis <daniel.amorim@stone.com.br>
2020-11-02 17:26:00 -0300

    Adding IRuneterraInternalClient

---
+++ src/LoR.Client/Internal/IRuneterraInternalClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Refit;
+
+namespace LoR.Client.Internal
+{
+    public interface IRuneterraInternalClient
+    {
+        [Get("/static-decklist")]
+        Task<ActiveDeckResponse> GetActiveDeckAsync();
+
+        [Get("/positional-rectangles")]
+        Task<CardPositionsResponse> GetCardPositions();
+
+        [Get("/expeditions-state")]
+        Task<ExpeditionsResponse> GetExpeditionsState();
+
+        [Get("/game-result")]
+        Task<GameResultResponse> GetLastGameResult();
+    }
+}
