49e4d3b3cdd417e2ae6e58bfa7748a1be711df33
Daniel Reis <daniel.amorim@stone.com.br>
2020-11-17 23:06:54 -0300

    Adding Card record

--- src/LoR.Client/Resources.cs
+++ src/LoR.Client/Resources.cs
@@ -13,4 +13,12 @@
     public record Rarity(string Name, string NameRef) : NamedResource(Name, NameRef) { }
 
     public record CardSet(string IconAbsolutePath, string Name, string NameRef) : NamedResource(Name, NameRef) { }
+
+    public record Asset(string GameAbsolutePath, string FullAbsolutePath);
+
+    public record Card(string[] AssociatedCards, string[] AssociatedCardRefs, Asset[] Assets, string Region, string RegionRef,
+        int Attack, int Cost, int Health, string Description, string DescriptionRaw, string LevelUpDescription, string LevelUpDescriptionRaw,
+        string FlavorText, string ArtistName, string Name, string CardCode, string[] Keywords, string[] KeywordRefs,
+        string SpellSpeed, string SpellSpeedRef, string Rarity, string RarityRef, string Subtype, string[] Subtypes, string Type,
+        bool Collectible, string Set);
 }
